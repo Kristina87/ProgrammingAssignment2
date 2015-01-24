81dcc9b859eb7fb88a8a0d2432b58cdadf995ecd
Kristina87 <kappa_g@hotmail.it>
2015-01-24 16:53:13 +0100

    example added

--- cachematrix.R
+++ cachematrix.R
@@ -50,5 +50,13 @@ cacheSolve <- function(x, ...)
 B = matrix(c(2, 4, 3, 1), nrow=2,ncol=2)
 myMatrix <- makeCacheMatrix(B)
 cacheSolve(myMatrix)
+#     [,1] [,2]
+#[1,] -0.1  0.3
+#[2,]  0.4 -0.2
+
 #in the case there is already the result
 cacheSolve(myMatrix)
+#getting cached data
+#     [,1] [,2]
+#[1,] -0.1  0.3
+#[2,]  0.4 -0.2
